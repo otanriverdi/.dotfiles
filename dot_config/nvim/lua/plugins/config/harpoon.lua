118cbafa2e435b2d857d8152210d2d8671babf60
Ozgur Tanriverdi <ozgur.tanriverdi94@gmail.com>
2024-01-01 20:42:40 +0300

    switch to harpoon 2

--- dot_config/nvim/lua/plugins/config/harpoon.lua
+++ dot_config/nvim/lua/plugins/config/harpoon.lua
@@ -1,24 +1,30 @@
 return {
-  "ThePrimeagen/harpoon",
-  config = function()
-    require("harpoon").setup({
-      tabline = false,
-    })
-  end,
-  init = function()
-    local mappings = {
-      n = {
-        ["<leader>ma"] = { ":lua require('harpoon.mark').add_file()<CR>", "add file to harpoon" },
-        ["<leader>mr"] = { ":lua require('harpoon.mark').rm_file()<CR>", "remove file from harpoon" },
-        ["<leader>mf"] = { ":lua require('harpoon.ui').toggle_quick_menu()<CR>", "toggle harpoon list" },
-      },
-    }
+	"ThePrimeagen/harpoon",
+	branch = "harpoon2",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+	},
+	config = function()
+		require("harpoon").setup()
+	end,
+	init = function()
+		local mappings = {
+			n = {
+				["<leader>ma"] = { ":lua require('harpoon'):list():append()<CR>", "add file to harpoon" },
+				["<leader>mf"] = {
+					":lua require('harpoon').ui:toggle_quick_menu(require('harpoon'):list())<CR>",
+					"toggle harpoon list",
+				},
+			},
+		}
 
-    for i = 1, 9 do
-      mappings.n[string.format("<leader>%s", i)] =
-        { string.format(":lua require('harpoon.ui').nav_file(%s)<CR>", i), string.format("jump to harpoon file %s", i) }
-    end
+		for i = 1, 9 do
+			mappings.n[string.format("<leader>%s", i)] = {
+				string.format(":lua require('harpoon'):list():select(%s)<CR>", i),
+				string.format("jump to harpoon file %s", i),
+			}
+		end
 
-    require("core.utils").load_mappings(mappings)
-  end,
+		require("core.utils").load_mappings(mappings)
+	end,
 }
